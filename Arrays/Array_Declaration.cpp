24b22dcde069eff201fbf314e9209a973c458df9
Itsrajsk <rajkashid146303@gmail.com>
2024-02-27 17:49:27 +0530

    Added Array Declaration

---
+++ Arrays/Array_Declaration.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* Array Declaration In C++
+    Data-Type Name[Size];
+    */
+
+    int arr[5];
+
+    char arr[10];
+
+    float arr[15];
+
+    // Static Array Declaration
+
+    int array[5] = {1, 2, 3, 4, 5};
+
+    // Dynamic Array Declaration
+
+    int *array = new int[5];
+    
+    return 0;
+}
