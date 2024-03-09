abf3a1cec63baf43bc59aa7ea154d6e363fcff30
Itsrajsk <rajkashid146303@gmail.com>
2024-03-09 21:44:10 +0530

    Added Reverse String Using Stack

---
+++ Stack/Reverse-String-Using-Stack.cpp
@@ -0,0 +1,31 @@
+//  Reverse String Using Stack
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<char> s;
+
+    string str = "Raj";
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        char ch = str[i];
+        s.push(ch);
+    }
+
+    string ans = "";
+
+    while (!s.empty())
+    {
+        char ch = s.top();
+        ans.push_back(ch);
+        s.pop();
+    }
+
+    cout << "Reverse String Is : " << ans << endl;
+
+    return 0;
+}
